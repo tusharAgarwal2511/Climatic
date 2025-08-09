9985edc4f6c7ad44c8ab18d952c098a8b3673e34
tusharAgarwal2511 <agarwal25112000@gmail.com>
2025-08-10 01:33:27 +0530

    fixed link name error

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/Climatic",
+    base: "/Climatic/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
