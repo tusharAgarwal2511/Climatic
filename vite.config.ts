e42fa3e8719e15fae19d3df0636875cef23a8408
tusharAgarwal2511 <agarwal25112000@gmail.com>
2025-08-10 01:25:49 +0530

    update package.json for gitHub pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: "/climatic",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
